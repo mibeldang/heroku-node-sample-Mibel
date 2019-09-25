f67b9d6ed37699e4a4f9358789d9ae7c6e4bf21a
mibeldang <mibeldangculos@gmail.com>
2019-09-25 09:06:54 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+
+var app = require('express')();
+var express = require('express');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static('public'));
+
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
+    console.log(msg)
+  });
+});
+
+http.listen(port, function () {
+  console.log('listening on *:' + port);
+});
+
+
+
+//listen on every connection
+io.on('connection', (socket) => {
+  console.log('New user connected')
+
+  //default username
+  socket.username = "Anonymous"
+
+  //listen on change_username
+  socket.on('change_username', (data) => {
+    socket.username = data.username
+    console.log(socket.username)
+  })
+
+  socket.on('log_in', (data) => {
+    io.sockets.emit('log_in', data);
+  })
+
+  socket.on('request_user', (data) => {
+    io.sockets.emit('request_user', data);
+  })
+
+  //listen on new_message
+  socket.on('new_message', (data) => {
+    //broadcast the new message
+    io.sockets.emit('new_message', data);
+  })
+
+  //listen on typing
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', data);
+  })
+
+})
---
+++ public/script.js
@@ -0,0 +1,82 @@
+
+
+var myName = "";
+var users = [""];
+$(document).ready(function () {
+  var socket = io();
+  socket.emit("request_user", "REQUEST");
+  var message = $("#message");
+  var username = $("#username");
+  var btnMessage = $("#btnMessage");
+  var btnUsername = $("#btnUsername");
+  var inbox = $("#inbox");
+  var feedback = $("#feedback");
+  var interval = 0;
+
+  //If username is saved
+  $(document).delegate('#btnUsername', 'click', function (e) {
+    myName = $('#username').val();
+    if (myName != "" && !users.includes(myName)) {
+      users.push(myName);
+      socket.emit("log_in", myName);
+      $(this).closest('.modal').removeClass('active');
+      e.preventDefault();
+      inbox.append("<i>You joined the group<br><i>");
+    } else {
+      alert("Username already used!!!");
+    }
+  });
+
+  socket.on('request_user', function (user) {
+    socket.emit("log_in", myName);
+  })
+
+  socket.on('log_in', function (user) {
+    console.log(user )
+    if (!users.includes(user)) {
+      users.push(user);
+      $('#users').append('<p> 🔵 <i>' + user + '</p>');
+      inbox.append("<i>" +user + " joined the group<i><br>");
+    }
+  })
+
+  //Emit message
+  btnMessage.click(function () {
+    socket.emit('new_message', { username: myName, message: $('#message').val() });
+    message.val('');
+  });
+
+  //Listen on new_message
+  socket.on("new_message", (data) => {
+    feedback.html('');
+    if (data.username == myName) {
+      inbox.append("<h4 style='color: black; margin-top:-10px' id='ownMessage'>" 
+      + data.message + "</h4><br><p style='color:black' id='ownMessageTime'>" +
+       moment().format('MMM Do YYYY, h:mm:ss a') + '</p>')
+    } else {
+      inbox.append("<h4 style='color: blue; margin-top:-10px' id='othersMessage'><b>" + 
+      data.username + ":</b>   " + data.message + "</h4><br><p style='color:green' id='otherMessageTime'>" +
+       moment().format('MMM Do YYYY, h:mm:ss a') + '</p>')
+    }
+    $("#inbox").scrollTop($("#inbox")[0].scrollHeight);
+  })
+
+  //Emit a username
+  btnUsername.click(function () {
+    socket.emit('change_username', { username: username.val() })
+  })
+
+  //Emit typing
+  message.on("keydown", () => {
+    socket.emit('typing', myName)
+  })
+
+  //Listen on typing
+  socket.on('typing', (data) => {
+    clearInterval(interval);
+    feedback.html("<p><i>" + data + " is typing a message..." + "</i></p>");
+    interval = setInterval(function () {
+      feedback.html('');
+    }, 2000);
+  })
+});
